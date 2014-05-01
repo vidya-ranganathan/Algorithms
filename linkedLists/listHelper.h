04fef79d6dfd0c9c8062c0f5b3d92863d6171b77
vidya-ranganathan <vidya-ranganathan@users.noreply.github.com>
2014-05-01 16:38:11 +0530

    Helper to create linked List and displays it
    Lets to create linked list node at nth position and display the entire list.

---
+++ linkedLists/listHelper.h
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<malloc.h>
+
+        struct linkList{
+                int data;
+                struct linkList *next;
+        };
+
+void displayList(struct linkList *node){
+
+        while(node->next != NULL){
+                printf("%d..", node->data);
+                node = node->next;
+        }
+                printf("%d..", node->data);
+		puts("\n");
+}
+
+void InsertLinkedList(struct linkList **head, int data, int pos) {
+	struct linkList *locHead, *locTail, *newNode;
+	int currPos = 1;
+	locHead = locTail = *head;
+
+	newNode = (struct linkList *)malloc(sizeof(struct linkList));
+	if(newNode == NULL) {
+		printf("NOMEM\n");
+		return;
+	}
+
+	newNode->data = data;
+	newNode->next = NULL;
+
+	if(pos == 1 && *head == NULL)
+		locHead = locTail = newNode;
+	else if(pos == 1) 
+	{
+		newNode->next = locHead;
+		locHead = newNode;
+	}
+	else {
+		while((locTail != NULL) && (currPos <= pos - 1))
+		{
+			currPos++;
+			if(locTail->next == NULL) {
+				locTail->next = newNode;
+			}
+			else if(currPos == pos) {
+				struct linkList *tempHead;
+				tempHead = locTail->next;
+				locTail->next = newNode;
+				newNode->next = tempHead;
+			}
+			locTail = locTail->next;
+		}
+	}
+	*head = locHead;
+}
